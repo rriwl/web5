998e264f94646e810e400cef81e8a4bc38d2be5c
Oleynik <144447072+rriwl@users.noreply.github.com>
2023-11-20 19:56:04 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function calculateTotal() {
+    /*количество товара*/
+    /*getElementById - метод, позволяющий получить ссылку на элемент страницы по идентификатору */
+    var quantity = document.getElementById("quantity").value; /*ищем элемент с идентификатором "quantity"*/
+    /*цена товара*/
+    var productPrice = document.getElementById("product").value; /*ищем элемент с идентификатором "product"*/
+    /*итоговая цена, умножение кол-ва товара на его цену*/
+    var totalCost = quantity * productPrice;
+    /*что должно выводиться*/
+    /*.innerHTML - позволяет получить доступ к содержимому эл-та*/
+    document.getElementById("totalCost").innerHTML = "Стоимость заказа: " + totalCost + " рублей";
+}
