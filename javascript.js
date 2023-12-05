830ec240a73d607a45c14a150af066772287ced4
Oleynik <144447072+rriwl@users.noreply.github.com>
2023-12-05 21:56:57 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,12 +1,16 @@
-function calculateTotal() {
+function calculateTotal() { 
     /*количество товара*/
     /*getElementById - метод, позволяющий получить ссылку на элемент страницы по идентификатору */
     var quantity = document.getElementById("quantity").value; /*ищем элемент с идентификатором "quantity"*/
+    if (isNaN(quantity) || quantity < 0) {
+        alert("Введите корректное количество товара");
+        return;
+    }
     /*цена товара*/
     var productPrice = document.getElementById("product").value; /*ищем элемент с идентификатором "product"*/
     /*итоговая цена, умножение кол-ва товара на его цену*/
-    var totalCost = quantity * productPrice;
+    var totalCost = quantity * productPrice; 
     /*что должно выводиться*/
     /*.innerHTML - позволяет получить доступ к содержимому эл-та*/
-    document.getElementById("totalCost").innerHTML = "Стоимость заказа: " + totalCost + " рублей";
+    document.getElementById("totalCost").innerHTML = "Стоимость заказа: " + totalCost + " рублей"; 
 }
